6d188e576cc7408339e44afa7a343bbb8f51d5c6
Muhammad Zeeshan Karamat <muhammadzeeshan020@gmail.com>
2015-09-19 18:32:47 -0700

    Changes

--- SMSAlert/app/src/main/java/xeaphii/com/smsalert/DesignSms.java
+++ SMSAlert/app/src/main/java/xeaphii/com/smsalert/DesignSms.java
@@ -7,9 +7,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import jxl.Cell;
@@ -24,6 +26,9 @@ public class DesignSms extends Activity {
     Button SendMessage,AddColumn;
     EditText MessageBody;
     Spinner ColumnsForExcel;
+    Sheet TempSheet = null;
+    String PhoneNumberIndex = "";
+    List<String> SmsList;
 
 
     @Override
@@ -43,26 +48,70 @@ public class DesignSms extends Activity {
                 MessageBody.setSelection(MessageBody.getText().length());
             }
         });
+        SendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String SmsBody = MessageBody.getText().toString().trim();
+                SmsList = new ArrayList<>(Collections.nCopies(TempSheet.getRows(), SmsBody));
+
+                int currentIndex =0 ;
+
+
+                 for (int j = 0; j < TempSheet.getRows(); j++) {
+                Cell cell = TempSheet.getCell(0, 0);
+                //if(cell.getContents().equalsIgnoreCase(key)){
+                for (int i = 0; i < TempSheet.getColumns(); i++) {
+                    if(i != Integer.parseInt(PhoneNumberIndex)) {
+                        Cell cel = TempSheet.getCell(i, 0);
+//                        resultSet.add(cel.getContents() + "");
+
+                    }
+                }
+                }
+                int start = 0 , end = 0;
+                while(currentIndex < MessageBody.getText().toString().trim().length()) {
+                    start = SmsBody.indexOf('{') + 1+currentIndex;
+                    end=SmsBody.indexOf('}')+currentIndex;
+                    int index = 0;
+                    String columnName = MessageBody.getText().toString().substring(start,end);
+                    for(int i =0 ; i < TempSheet.getColumns();i++){
+                        if(columnName.equals(TempSheet.getCell(i, 0).getContents())) {
+//                            Cell cel = TempSheet.getCell(i, 0);
+//                        resultSet.add(cel.getContents() + "");
+                            index= i;
+                            break;
+                        }
+                    }
+                    for(int i = 1 ; i < SmsList.size();i++){
+                        SmsList.set(i,SmsList.get(i).substring(start-1)+TempSheet.getCell(index, i).getContents()+SmsList.get(i).substring(end+1)) ;
+                    }
+                    //output = SmsBody.indexOf('}');
+                    currentIndex += SmsBody.indexOf('}')+1;
+                    SmsBody= SmsBody.substring(SmsBody.indexOf('}')+1,SmsBody.length());
+                }
+            }
+        });
 
         List<String> resultSet = new ArrayList<String>();
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             String FilePath = extras.getString("FilePath");
-            String PhoneNumberIndex = extras.getString("numberColumn");
+            PhoneNumberIndex = extras.getString("numberColumn");
             File inputWorkbook = new File(FilePath);
                 if (inputWorkbook.exists()) {
                     Workbook w;
                     try {
                         w = Workbook.getWorkbook(inputWorkbook);
                         // Get the first sheet
-                        Sheet sheet = w.getSheet(0);
+                        TempSheet = w.getSheet(0);
                         // Loop over column and lines
                         // for (int j = 0; j < sheet.getRows(); j++) {
-                        Cell cell = sheet.getCell(0, 0);
+                        Cell cell = TempSheet.getCell(0, 0);
                         //if(cell.getContents().equalsIgnoreCase(key)){
-                        for (int i = 0; i < sheet.getColumns(); i++) {
+                        for (int i = 0; i < TempSheet.getColumns(); i++) {
                             if(i != Integer.parseInt(PhoneNumberIndex)) {
-                                Cell cel = sheet.getCell(i, 0);
+                                Cell cel = TempSheet.getCell(i, 0);
                                 resultSet.add(cel.getContents() + "");
                             }
                         }
