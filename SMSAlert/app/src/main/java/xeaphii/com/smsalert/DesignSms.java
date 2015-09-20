2f16181638b7db31eedf2646c190b90b92a317f7
Muhammad Zeeshan Karamat <muhammadzeeshan020@gmail.com>
2015-09-20 02:22:45 -0700

    Bug fixed

--- SMSAlert/app/src/main/java/xeaphii/com/smsalert/DesignSms.java
+++ SMSAlert/app/src/main/java/xeaphii/com/smsalert/DesignSms.java
@@ -283,7 +283,7 @@ public class DesignSms extends Activity {
 
                         mMessageSentParts++;
                         if ( mMessageSentParts == mMessageSentTotalParts ) {
-                          //  mMessageSentCount++;
+                            mMessageSentCount++;
                             sendNextMessage();
                         }
 
